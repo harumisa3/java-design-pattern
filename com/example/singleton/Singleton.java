c21fa5de975a9438b90e4f3c310db96ffe32dab1
mikemia3 <mikemia3@gmail.com>
2021-08-08 14:22:59 +0900

    singletonパターンの追加

---
+++ com/example/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package com.example.singleton;
+
+public class Singleton {
+
+  // static変数
+  private static Singleton singleton = new Singleton();
+
+  // コンストラクタ
+  private Singleton() {
+    System.out.println("インスタンスを生成しました");
+  }
+
+  // インスタンス取得用のstaticメソッド
+  public static Singleton getInstance() {
+    return singleton;
+  }
+}
