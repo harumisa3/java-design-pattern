60cc4f8d3998102d2bf9f2a1b5214a1d2186c59b
mikemia3 <mikemia3@gmail.com>
2021-08-09 21:49:43 +0900

    decoratorパターンの追加

---
+++ com/example/decorator/IceCream.java
@@ -0,0 +1,10 @@
+package com.example.decorator;
+
+public abstract class IceCream {
+
+  // 価格を取得するメソッド
+  public abstract int getPrice();
+
+  // 名称を取得するメソッド
+  public abstract String getName();
+}
